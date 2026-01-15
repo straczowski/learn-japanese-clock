2f48483848a9cee55ba490c7e82132f289f82c99
Raoul Straczowski <raoulstraczowski@Raouls-MacBook-Pro.local>
2026-01-15 19:58:10 +0100

    client side cache of audio files

--- src/utils/play-sound.ts
+++ src/utils/play-sound.ts
@@ -1,8 +1,3 @@
-const playSound = (filename: string) => {
-  const audio = new Audio(`/sound/action/${filename}`)
-  audio.play()
-}
-
 export const playSuccessSound = () => {
   playSound('success.mp3')
 }
@@ -11,12 +6,31 @@ export const playFailSound = () => {
   playSound('fail.mp3')
 }
 
+const playSound = (filename: string) => {
+  const audio = getOrCreateAudio(`/sound/action/${filename}`)
+  audio.play()
+}
+
 export const playExpression = (timeId: string, romaji: string) => {
   const soundId = getSoundId(timeId, romaji)
-  const audio = new Audio(`/public/sound/voice/${soundId}.webm`)
+  const audio = getOrCreateAudio(`/sound/voice/${soundId}.webm`)
   audio.play()
 }
 
 const getSoundId = (timeId: string, romaji: string): string => {
   return `${timeId}_${romaji.replaceAll(" ", "_")}`
+}
+
+const audioCache = new Map<string, HTMLAudioElement>()
+const getOrCreateAudio = (url: string): HTMLAudioElement => {
+  if (audioCache.has(url)) {
+    const cachedAudio = audioCache.get(url)
+    if (cachedAudio) {
+      cachedAudio.currentTime = 0
+      return cachedAudio
+    }
+  }
+  const audio = new Audio(url)
+  audioCache.set(url, audio)
+  return audio
 }
